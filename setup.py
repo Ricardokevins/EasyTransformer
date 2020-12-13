6eea74cedb6e00a3dd0c644651e6a9299088f408
Ricardokevins <3121416933@qq.com>
2020-12-13 13:04:28 +0800

    update

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='EasyTransformer',
+    version='0.0.2',
+    author='kevinpro',
+    author_email='3121416933@qq.com',
+    url='https://github.com/Ricardokevins',
+    description=u'Simple implement of BERT and Transformer extracted from other repo',
+    packages=['EasyTransformer'],
+    install_requires=['numpy','torch'],
+)
+#python setup.py sdist build
+#twine upload dist/*
